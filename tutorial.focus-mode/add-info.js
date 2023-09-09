c94fc2218cc07cc517c425f45c742fd31b4b4642
Yufeng Zhao <yufeng-zhao@outlook.com>
2023-09-09 19:16:36 -0400

    adding style polishes and fix copies

--- tutorial.focus-mode/add-info.js
+++ tutorial.focus-mode/add-info.js
@@ -1,31 +1,31 @@
-console.log("Adding info");
+console.log('Adding info');
 
-let OPENAI_API_KEY = "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";
+let OPENAI_API_KEY = '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';
 
 function getArticles() {
-  const articles = [...document.querySelectorAll("li.arxiv-result")].map(
+  const articles = [...document.querySelectorAll('li.arxiv-result')].map(
     (article) => {
-      const title = article.querySelector("p.title.is-5.mathjax").innerText;
+      const title = article.querySelector('p.title.is-5.mathjax').innerText;
       const authors = article
-        .querySelector("p.authors")
-        .innerText.replace("\n", " ")
-        .replace("Authors: ", "")
-        .split(", ");
+        .querySelector('p.authors')
+        .innerText.replace('\n', ' ')
+        .replace('Authors: ', '')
+        .split(', ');
 
       const abstract = article
-        .querySelector("span.abstract-full.has-text-grey-dark.mathjax")
-        .innerText.replace("\n", " ")
-        .replace("Abstract: ", "")
-        .replace("△ Less", "")
+        .querySelector('span.abstract-full.has-text-grey-dark.mathjax')
+        .innerText.replace('\n', ' ')
+        .replace('Abstract: ', '')
+        .replace('△ Less', '')
         .trim();
 
       const submittedDateString = article
-        .querySelector("p.is-size-7")
-        .innerText.replace("\n", " ")
-        .replace("Submitted ", "")
-        .replace("originally announced", "")
-        .replace(".", "")
-        .split(";")[0]
+        .querySelector('p.is-size-7')
+        .innerText.replace('\n', ' ')
+        .replace('Submitted ', '')
+        .replace('originally announced', '')
+        .replace('.', '')
+        .split(';')[0]
         .trim();
 
       const submittedDate = dateStringToDataTime(submittedDateString);
@@ -33,17 +33,17 @@ function getArticles() {
       //  23 July, 2021
       function dateStringToDataTime(dateString) {
         const [month, day, year] = dateString
-          .split(" ")
-          .map((item) => item.replace(",", ""));
+          .split(' ')
+          .map((item) => item.replace(',', ''));
         return new Date(`${year}-${month}-${day}`);
       }
 
       const articleLink = article.querySelector(
-        "div.is-marginless p.list-title a"
+        'div.is-marginless p.list-title a'
       ).href;
 
       const pdfLink = article.querySelector(
-        "div.is-marginless p.list-title span a"
+        'div.is-marginless p.list-title span a'
       ).href;
 
       return {
@@ -64,7 +64,7 @@ function getUserSearchKeywords() {
   // const queryString = window.location.search;
   // const urlParams = new URLSearchParams(queryString);
   // const userSearchKeyWords = urlParams.get("query").replace("+", " ");
-  return "";
+  return '';
 }
 
 /*
@@ -97,19 +97,19 @@ async function summarizeArticle(title, abstract, userSearchKeyWords) {
   }
 
   const apiKey = OPENAI_API_KEY;
-  const url = "https://api.openai.com/v1/chat/completions";
+  const url = 'https://api.openai.com/v1/chat/completions';
 
   const headers = {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
     Authorization: `Bearer ${apiKey}`,
   };
 
   const data = JSON.stringify({
-    model: "gpt-3.5-turbo",
+    model: 'gpt-3.5-turbo',
     messages: [
-      { role: "system", content: systemContext },
+      { role: 'system', content: systemContext },
       {
-        role: "user",
+        role: 'user',
         content: composeLLMInputData(title, abstract, userSearchKeyWords),
       },
     ],
@@ -118,26 +118,26 @@ async function summarizeArticle(title, abstract, userSearchKeyWords) {
 
   console.log(`Requesting Summary of ${title}.`);
   const response = await fetch(url, {
-    method: "POST",
+    method: 'POST',
     headers: headers,
     body: data,
   });
 
   const result = await response.json();
-  console.log("result", result);
+  console.log('result', result);
   const messageContent = result.choices[0].message.content;
-  console.log("messageContent", result.choices[0].message.content);
-  const processedMessageContent = messageContent.replace(/[\r\n]+/gm, "");
-  console.log("processedMessageContent", processedMessageContent);
+  console.log('messageContent', result.choices[0].message.content);
+  const processedMessageContent = messageContent.replace(/[\r\n]+/gm, '');
+  console.log('processedMessageContent', processedMessageContent);
   const parsedResult = JSON.parse(messageContent);
   return parsedResult;
 }
 
 async function main() {
   const articles = getArticles();
-  const articleList = document.querySelector("ol.breathe-horizontal");
+  const articleList = document.querySelector('ol.breathe-horizontal');
   const userSearchKeyWords = getUserSearchKeywords();
-  console.log("All articles", articles);
+  console.log('All articles', articles);
   for (let i = 0; i < 2; i++) {
     const firstArticle = articles[i];
     try {
@@ -153,22 +153,22 @@ async function main() {
       // newSummary.className = 'authors';
       // firstArticle.domReference.appendChild(newSummary);
 
-      const newKeywords = document.createElement("li");
+      const newKeywords = document.createElement('li');
       // newKeywords.innerHTML = `<span class="has-text-black-bis has-text-weight-semibold">Suggested Keywords:</span> ${firstSummary.keywords.join(
       //   ", "
       // )}`;
       // newKeywords.className = "authors";
       // firstArticle.domReference.appendChild(newKeywords);
-      newKeywords.className = "keywords-container";
+      newKeywords.className = 'keywords-container';
       firstSummary.keywords.forEach((keyword) => {
-        const newKeyword = document.createElement("button");
+        const newKeyword = document.createElement('button');
         newKeyword.textContent = keyword;
         newKeyword.className =
-          "rounded-sm bg-zinc-100 hover:bg-zinc-200 px-2 py-1 m-0.5 text-sm border-2 border-black";
-        newKeyword.addEventListener("click", () => {
-          chrome.storage.local.get("selected_keywords").then((result) => {
+          'rounded-sm bg-zinc-100 hover:bg-zinc-200 px-2 py-1 m-0.5 text-sm border-2 border-black';
+        newKeyword.addEventListener('click', () => {
+          chrome.storage.local.get('selected_keywords').then((result) => {
             const selectedKeywords = result.selected_keywords || [];
-            console.log("previous selected keywords", selectedKeywords);
+            console.log('previous selected keywords', selectedKeywords);
             const storedKeywordIndex = selectedKeywords.findIndex(
               (storedKeyword) => storedKeyword.keyword === keyword
             );
@@ -182,7 +182,7 @@ async function main() {
                   top: Math.floor(Math.random() * 200 + 635),
                   left: 0,
                 },
-                type: "leftout",
+                type: 'leftout',
               });
             }
             chrome.storage.local.set({
@@ -208,18 +208,18 @@ async function main() {
       // newRelativeTopics.className = 'authors';
       // firstArticle.domReference.appendChild(newRelativeTopics);
     } catch (e) {
-      console.log("error", e);
+      console.log('error', e);
     }
   }
 }
 
 // get the input from the user
-const searchInput = document.querySelector("input#query");
+const searchInput = document.querySelector('input#query');
 // add the input to the storage
-chrome.storage.local.get("searchInput", (result) => {
-  const storedSearchInput = result.searchInput || "";
+chrome.storage.local.get('searchInput', (result) => {
+  const storedSearchInput = result.searchInput || '';
   storedSearchInput.push({ text: searchInput.value });
-  console.log("storedSearchInput", storedSearchInput);
+  console.log('storedSearchInput', storedSearchInput);
   // remove all duplicates
   const uniqueStoredSearchInput = storedSearchInput.filter(
     (item, index, self) => index === self.findIndex((t) => t.text === item.text)
--- tutorial.focus-mode/sidepanel.js
+++ tutorial.focus-mode/sidepanel.js
@@ -1,22 +1,32 @@
 // sidebar.js
 
+const dropArea1Range = {
+  top: 200,
+  bottom: 390,
+};
+
+const dropArea2Range = {
+  top: 425,
+  bottom: 615,
+};
+
 // Tabs
 const showPage1 = () => {
-  document.getElementById("page1").style.display = "block";
-  document.getElementById("tab1").style.backgroundColor = "steelblue";
-  document.getElementById("page2").style.display = "none";
-  document.getElementById("tab2").style.backgroundColor = "lightblue";
+  document.getElementById('page1').style.display = 'block';
+  document.getElementById('tab1').style.backgroundColor = 'steelblue';
+  document.getElementById('page2').style.display = 'none';
+  document.getElementById('tab2').style.backgroundColor = 'lightblue';
 };
 const showPage2 = () => {
-  document.getElementById("page2").style.display = "block";
-  document.getElementById("tab2").style.backgroundColor = "steelblue";
-  document.getElementById("page1").style.display = "none";
-  document.getElementById("tab1").style.backgroundColor = "lightblue";
+  document.getElementById('page2').style.display = 'block';
+  document.getElementById('tab2').style.backgroundColor = 'steelblue';
+  document.getElementById('page1').style.display = 'none';
+  document.getElementById('tab1').style.backgroundColor = 'lightblue';
 };
-document.getElementById("tab1").addEventListener("click", function () {
+document.getElementById('tab1').addEventListener('click', function () {
   showPage1();
 });
-document.getElementById("tab2").addEventListener("click", function () {
+document.getElementById('tab2').addEventListener('click', function () {
   showPage2();
 });
 showPage1();
@@ -28,23 +38,23 @@ let state = {
   searched_keywords: [],
 };
 
-var WordDragType = "default" | "quoted" | "leftout";
+var WordDragType = 'default' | 'quoted' | 'leftout';
 
 var Keyword = {
-  keyword: "",
+  keyword: '',
   count: 0,
   position: { top: 0, left: 0 },
   type: WordDragType,
 };
 
 var article_history = {
-  id: "",
-  title: "",
-  url: "",
+  id: '',
+  title: '',
+  url: '',
 };
 
 var searched_keywords = {
-  text: "",
+  text: '',
 };
 
 // synchronize the storage with the elements on the page
@@ -55,12 +65,15 @@ function synchronizeKeywordPosition(keywordObj) {
   keywordObj.position.top = element.style.top;
   keywordObj.position.left = element.style.left;
   topValue = parseInt(keywordObj.position.top.slice(0, -2));
-  if (topValue > 150 && topValue < 340) {
-    keywordObj.type = "default";
-  } else if (topValue > 375 && topValue < 565) {
-    keywordObj.type = "quoted";
+  if (topValue > dropArea1Range.top && topValue < dropArea1Range.bottom) {
+    keywordObj.type = 'default';
+  } else if (
+    topValue > dropArea2Range.top &&
+    topValue < dropArea2Range.bottom
+  ) {
+    keywordObj.type = 'quoted';
   } else {
-    keywordObj.type = "leftout";
+    keywordObj.type = 'leftout';
   }
   // console.log("keywordObj", keywordObj);
   return keywordObj;
@@ -120,41 +133,41 @@ function synchronizeKeywordPosition(keywordObj) {
 
 // updateOneKeywordInState(keyword: Keyword) {
 
-chrome.storage.local.get("selected_keywords").then((result) => {
+chrome.storage.local.get('selected_keywords').then((result) => {
   const selectedKeywords = result.selected_keywords || [];
   state.selected_keywords = selectedKeywords;
-  console.log("previous selected keywords", selectedKeywords);
+  console.log('previous selected keywords', selectedKeywords);
   reloadKeywords(selectedKeywords);
   // input text
   createInput(selectedKeywords);
   clearKeywordsButton();
 });
 
-chrome.storage.local.get("history").then((result) => {
+chrome.storage.local.get('history').then((result) => {
   const article_history = result.history || [];
   state.article_history = article_history;
-  console.log("previous article history", article_history);
-  populateArticleHistoryList("articleList", article_history);
+  console.log('previous article history', article_history);
+  populateArticleHistoryList('articleList', article_history);
 });
 
-chrome.storage.local.get("searchInput").then((result) => {
+chrome.storage.local.get('searchInput').then((result) => {
   const searched_keywords = result.searchInput || [];
   state.searched_keywords = searched_keywords;
-  console.log("previous searched keywords", searched_keywords);
-  populateSearchedKeywordsList("searchedList", searched_keywords);
+  console.log('previous searched keywords', searched_keywords);
+  populateSearchedKeywordsList('searchedList', searched_keywords);
 });
 
 chrome.storage.onChanged.addListener((message) => {
-  console.log("message", message);
+  console.log('message', message);
   if (message.history) {
     const newArticleHistory = message.history.newValue || [];
     state.article_history = newArticleHistory;
-    populateArticleHistoryList("articleList", newArticleHistory);
+    populateArticleHistoryList('articleList', newArticleHistory);
   }
   if (message.searchInput) {
     const newSearchedKeywords = message.searchInput.newValue || [];
     state.searched_keywords = newSearchedKeywords;
-    populateSearchedKeywordsList("searchedList", newSearchedKeywords);
+    populateSearchedKeywordsList('searchedList', newSearchedKeywords);
   }
   if (message.selected_keywords) {
     const newKeywords = message.selected_keywords.newValue || [];
@@ -174,36 +187,36 @@ setInterval(() => {
 
 function reloadKeywords(keywords) {
   // clean up the sidepanel
-  const selectedKeywords = document.getElementById("selected-keyword");
-  selectedKeywords.innerHTML = "";
+  const selectedKeywords = document.getElementById('selected-keyword');
+  selectedKeywords.innerHTML = '';
   // add the new keywords
-  console.log("keywords", keywords);
+  console.log('keywords', keywords);
   keywords.forEach((keywordObj) => {
     if (keywordObj) {
       addKeywordButton(keywordObj);
     }
   });
   chrome.storage.local.set({ selected_keywords: keywords }, () => {
-    console.log("state sent to storage", keywords);
+    console.log('state sent to storage', keywords);
   });
 }
 
 function addKeywordButton(keywordObj) {
   const keyword = keywordObj.keyword;
-  const selectedKeywords = document.getElementById("selected-keyword");
-  const newSelectedKeyword = document.createElement("div");
-  newSelectedKeyword.style = "position:absolute; z-index: 9";
+  const selectedKeywords = document.getElementById('selected-keyword');
+  const newSelectedKeyword = document.createElement('div');
+  newSelectedKeyword.style = 'position:absolute; z-index: 9';
   newSelectedKeyword.id = keyword;
-  newSelectedKeyword.className = "draggable";
+  newSelectedKeyword.className = 'draggable';
 
   newSelectedKeyword.style.top = keywordObj.position.top;
   newSelectedKeyword.style.left = keywordObj.position.left;
-  const header = document.createElement("div");
+  const header = document.createElement('div');
   header.appendChild(document.createTextNode(keyword));
   header.className =
-    "rounded-md bg-zinc-100 hover:bg-zinc-200 px-4 py-2 m-1 text-md border-2 border-black transition-colors duration-500 ease-in-out";
-  header.id = keyword + "header";
-  header.style = "cursor: move;z-index: 10";
+    'rounded-md bg-zinc-100 hover:bg-zinc-200 px-4 py-2 m-1 text-md border-2 border-black transition-colors duration-500 ease-in-out';
+  header.id = keyword + 'header';
+  header.style = 'cursor: move;z-index: 10';
   newSelectedKeyword.appendChild(header);
 
   // newSelectedKeyword.addEventListener("click", () => {
@@ -218,26 +231,30 @@ function addKeywordButton(keywordObj) {
 
 // Change Current Input Div every second
 function createInput(keywords) {
-  const inputKeyword = document.getElementById("currentInput");
-  let newInput = "";
-  keywords.forEach((keywordObj) => {
+  const inputKeyword = document.getElementById('currentInput');
+  let newInput = '';
+  keywords.forEach((keywordObj, i) => {
     if (keywordObj) {
       synchronizeKeywordPosition(keywordObj);
-      if (keywordObj.type === "default") {
-        newInput += keywordObj.keyword;
-        newInput += " & ";
-      } else if (keywordObj.type === "quoted") {
-        newInput += '"';
-        newInput += keywordObj.keyword;
-        newInput += '" & ';
+      if (keywordObj.type === 'default') {
+        newInput += `<div><span class="rounded-xl bg-blue-500 px-2 py-1 font-bold text-white">${
+          keywordObj.keyword
+        }</span> 
+        ${i !== keywords.length - 1 ? ' & ' : ''}</div>`;
+        // newInput += keywordObj.keyword;
+      } else if (keywordObj.type === 'quoted') {
+        newInput += `<div><span class="rounded-xl bg-green-500 px-2 py-1 font-bold text-white">"${
+          keywordObj.keyword
+        }"</span>
+        ${i !== keywords.length - 1 ? ' & ' : ''}</div>`;
       }
     }
   });
-  newInput = newInput.slice(0, -2);
+  // newInput = newInput.slice(0, -8);
   inputKeyword.innerHTML = newInput;
   // click addClassButton and newInput will be sent to background.js
-  const createButton = document.getElementById("addClassButton");
-  createButton.addEventListener("click", () => {
+  const createButton = document.getElementById('addClassButton');
+  createButton.addEventListener('click', () => {
     let msg = { chosen_keyword: newInput };
     chrome.runtime.sendMessage(msg);
   });
@@ -248,9 +265,9 @@ function dragElement(elmnt) {
     pos2 = 0,
     pos3 = 0,
     pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
+  if (document.getElementById(elmnt.id + 'header')) {
     // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    document.getElementById(elmnt.id + 'header').onmousedown = dragMouseDown;
   } else {
     // otherwise, move the DIV from anywhere inside the DIV:
     elmnt.onmousedown = dragMouseDown;
@@ -278,14 +295,17 @@ function dragElement(elmnt) {
     pos3 = e.clientX;
     pos4 = e.clientY;
     // set the element's new position:
-    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
-    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
-    let header = document.getElementById(elmnt.id + "header");
+    elmnt.style.top = elmnt.offsetTop - pos2 + 'px';
+    elmnt.style.left = elmnt.offsetLeft - pos1 + 'px';
+    let header = document.getElementById(elmnt.id + 'header');
     //150-340, 375-565
-    if ((pos4 > 150 && pos4 < 340) || (pos4 > 375 && pos4 < 565)) {
-      header.className = header.className.replace("bg-zinc", "bg-blue");
+    if (
+      (pos4 > dropArea1Range.top && pos4 < dropArea1Range.bottom) ||
+      (pos4 > dropArea2Range.top && pos4 < dropArea2Range.bottom)
+    ) {
+      header.className = header.className.replace('bg-zinc', 'bg-blue');
     } else {
-      header.className = header.className.replace("bg-blue", "bg-zinc");
+      header.className = header.className.replace('bg-blue', 'bg-zinc');
     }
   }
 
@@ -298,68 +318,68 @@ function dragElement(elmnt) {
 
 // Add listener to the clear button
 function clearKeywordsButton() {
-  const clearKeywordsButton = document.getElementById("clearKeywordsButton");
-  clearKeywordsButton.addEventListener("click", () => {
-    chrome.storage.local.get("selected_keywords").then((result) => {
+  const clearKeywordsButton = document.getElementById('clearKeywordsButton');
+  clearKeywordsButton.addEventListener('click', () => {
+    chrome.storage.local.get('selected_keywords').then((result) => {
       const selectedKeywords = result.selected_keywords || [];
       selectedKeywords.forEach((keywordObj) => {
         if (keywordObj) synchronizeKeywordPosition(keywordObj);
       });
       // check if the keyword's type is leftout and clear
       const newKeywords = selectedKeywords.filter(
-        (keyword) => keyword.type !== "leftout"
+        (keyword) => keyword.type !== 'leftout'
       );
       chrome.storage.local.set({ selected_keywords: newKeywords }, () => {
-        console.log("state sent to storage", newKeywords);
+        console.log('state sent to storage', newKeywords);
       });
     });
   });
 }
 
-let OPENAI_API_KEY = "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";
+let OPENAI_API_KEY = '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';
 
-const openDialogButton = document.getElementById("openDialogButton");
-const dialogBox = document.getElementById("dialogBox");
-const inputField = document.getElementById("inputField");
-const submitButton = document.getElementById("submitButton");
-const closeDialogButton = document.getElementById("closeDialogButton"); // Add this line
+const openDialogButton = document.getElementById('openDialogButton');
+const dialogBox = document.getElementById('dialogBox');
+const inputField = document.getElementById('inputField');
+const submitButton = document.getElementById('submitButton');
+const closeDialogButton = document.getElementById('closeDialogButton'); // Add this line
 
-openDialogButton.addEventListener("click", () => {
-  dialogBox.style.display = "block";
+openDialogButton.addEventListener('click', () => {
+  dialogBox.style.display = 'block';
 });
 
-closeDialogButton.addEventListener("click", () => {
-  dialogBox.style.display = "none";
-  inputField.value = "";
+closeDialogButton.addEventListener('click', () => {
+  dialogBox.style.display = 'none';
+  inputField.value = '';
 });
 
-submitButton.addEventListener("click", () => {
+submitButton.addEventListener('click', () => {
   const inputValue = inputField.value;
   if (inputValue) {
     askAI(inputValue).then((rep) => {
-      console.log("rep", rep);
-      aiDialog = document.getElementById("aiDialog");
+      console.log('rep', rep);
+      aiDialog = document.getElementById('aiDialog');
       aiDialog.innerHTML = rep;
       // Close the dialog box
-      inputField.value = "";
+      inputField.value = '';
     });
   }
 });
 
 async function askAI(userQuestion) {
-  console.log("state", state);
+  console.log('state', state);
   const selected_keywords = state.selected_keywords
     .filter(
-      (keyword) => keyword.type === "default" || keyword.type === "quoted"
+      (keyword) => keyword.type === 'default' || keyword.type === 'quoted'
     )
     .map((keyword) => keyword.keyword)
-    .join(", ");
+    .join(', ');
   const searched_keywords = state.searched_keywords
     .map((searches) => searches.text)
-    .join(", ");
+    .join(', ');
   const article_history = state.article_history
     .map((article) => article.title)
-    .join(", ");
+    .join(', ');
 
   let prompt =
     `Your are a helpful academic librarian. The student is doing some research and here's what they have so far:\n` +
@@ -369,46 +389,46 @@ async function askAI(userQuestion) {
     `Now they are thinking about: ${userQuestion}\n` +
     `You are going to help them by giving some advice or answering their question.\n` +
     `And please limit your response within 100 words.`;
-  console.log("prompt", prompt);
+  console.log('prompt', prompt);
   const apiKey = OPENAI_API_KEY;
-  const url = "https://api.openai.com/v1/chat/completions";
+  const url = 'https://api.openai.com/v1/chat/completions';
   const headers = {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
     Authorization: `Bearer ${apiKey}`,
   };
 
   const data = JSON.stringify({
-    model: "gpt-3.5-turbo",
+    model: 'gpt-3.5-turbo',
     messages: [
-      { role: "system", content: prompt },
+      { role: 'system', content: prompt },
       {
-        role: "user",
+        role: 'user',
         content: userQuestion,
       },
     ],
     temperature: 0.7,
   });
   const response = await fetch(url, {
-    method: "POST",
+    method: 'POST',
     headers: headers,
     body: data,
   });
   const result = await response.json();
   //console.log("result", result);
   const messageContent = result.choices[0].message.content;
-  console.log("messageContent", result.choices[0].message.content);
+  console.log('messageContent', result.choices[0].message.content);
   return messageContent;
 }
 
 // Function to populate a list with items including title and URL
 function populateArticleHistoryList(listId, items) {
   const list = document.getElementById(listId);
-  list.innerHTML = ""; // Clear previous items
+  list.innerHTML = ''; // Clear previous items
 
   for (let i = items.length - 1; i >= 0; i--) {
     const item = items[i];
-    const listItem = document.createElement("li");
-    const link = document.createElement("a");
+    const listItem = document.createElement('li');
+    const link = document.createElement('a');
     link.textContent = item.title;
     link.href = item.url;
     listItem.appendChild(link);
@@ -419,10 +439,10 @@ function populateArticleHistoryList(listId, items) {
 // Function to populate the searched keywords list
 function populateSearchedKeywordsList(listId, items) {
   const list = document.getElementById(listId);
-  list.innerHTML = ""; // Clear previous items
+  list.innerHTML = ''; // Clear previous items
 
   items.forEach((item) => {
-    const listItem = document.createElement("li");
+    const listItem = document.createElement('li');
     listItem.textContent = item.text;
     list.appendChild(listItem);
   });
